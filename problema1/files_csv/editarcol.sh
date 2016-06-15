b2e0b4551fe7fa05c95102978bfe05c311b6010c
mrciveniv <w79cr@yahoo.com>
2016-06-15 10:40:23 -0600

    agregando graficas con mas datos para mejor visualización

---
+++ problema1/files_csv/editarcol.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#script para aislar informacion de la quinta columna#########
+
+echo " Dando formato a los datos para poder graficar "
+echo " Mostrara solo las primeras las primeras 10 lineas "
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos1_csv.csv | sed '1 s/date/#date/g' | head -100 >> plot/infograficar1.gnu
+
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos2_csv.csv | sed '1 s/date/#date/g' | head -100 >> plot/infograficar2.gnu
+
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos3_csv.csv | sed '1 s/date/#date/g' | head -100 >> plot/infograficar3.gnu
+
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos4_csv.csv | sed '1 s/date/#date/g' | head -100 >> plot/infograficar4.gnu
+
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos5_csv.csv | sed '1 s/date/#date/g' | head -100 >> plot/infograficar5.gnu
+
+
+
+
+
