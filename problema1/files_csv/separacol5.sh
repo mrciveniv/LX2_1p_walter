281ae6e78b9443ed67ce7ceea4ff2d666f2063cc
mrciveniv <w79cr@yahoo.com>
2016-06-15 09:56:17 -0600

    info de las graficas

--- problema1/files_csv/separacol5.sh
+++ problema1/files_csv/separacol5.sh
@@ -2,16 +2,17 @@
 
 #script para aislar informacion de la quinta columna#########
 
-echo " Este script aisla solo la informacion de la columna 5 "
+echo " Dando formato a los datos para poder graficar "
 echo " Mostrara solo las primeras las primeras 10 lineas "
-awk -F"," '{ print $1,$2,$3,$4,$5 }' datos1_csv.csv | sed '1 s/date/#date/g' | head >> graficar.gnu
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos1_csv.csv | sed '1 s/date/#date/g' | head >> plot/infograficar1.gnu
 
- 
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos2_csv.csv | sed '1 s/date/#date/g' | head >> plot/infograficar2.gnu
 
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos3_csv.csv | sed '1 s/date/#date/g' | head >> plot/infograficar3.gnu
 
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos4_csv.csv | sed '1 s/date/#date/g' | head >> plot/infograficar4.gnu
 
-
-
+awk -F"," '{ print $1,$2,$3,$4,$5 }' datos5_csv.csv | sed '1 s/date/#date/g' | head >> plot/infograficar5.gnu
 
 
 
