89c07a942032eb0d36c98db2ffd7ef48a93ad082
mrciveniv <w79cr@yahoo.com>
2016-06-20 20:08:06 -0600

    plot del segundo problema

--- problema2/plot2/listo/plotgraficador.sh
+++ problema2/plot2/listo/plotgraficador.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/gnuplot
 
-reset
+
 set terminal png
 
-set output "plot1.png"
+set output "plot2.png"
 
 set xlabel "Mes"
 set xrange [1:6]
@@ -17,6 +17,10 @@ set title "Variacion en consumo Agua y Luz de enero a junio"
 
 set style data linespoints
 
-plot "plot1.dat" using 1:2 title "Luz", \
-"" using 1:3 title "Agua" 
+plot "datosgraf.csv" using 1:2 with lines title "Luz", "datosgraf.csv" using 1:3with lines title "Agua"
+
+
+
+ 
+
 
